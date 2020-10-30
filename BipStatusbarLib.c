01043797ef1bd5ef8a6025105961a2c673fd599e
ugers <yugers@gmail.com>
2020-10-30 22:05:22 +0300

    because we have now more space get alarm icon bigger. Simplify bluetooth code

--- BipStatusbarLib.c
+++ BipStatusbarLib.c
@@ -14,7 +14,7 @@
 	#include <libbip.h>
 #endif
 
-//ОПЦИИ
+//ОПЦИИ Options
 #define BATTERY_TEXT
 #define BATTERY_ICON
 #define TIME
@@ -32,10 +32,10 @@ void show_statusbar(int y, int bgColor,int fgColor){
 
 	//БАТАРЕЯ
 	#ifdef BATTERY_ICON
-	set_bg_color(COLOR_BLACK); // делаем фон
+	set_bg_color(COLOR_BLACK); // подложка
 	draw_filled_rect_bg(152, y, 172, y + 16); // сама батарейка
 	draw_filled_rect_bg(170, y + 4, 174, y + 12); //кончик
-	set_bg_color(COLOR_WHITE); // делаем фон
+	set_bg_color(COLOR_WHITE); //корпус
 	draw_filled_rect_bg(153, y + 1, 171, y + 15); // сама батарейка
 	draw_filled_rect_bg(171, y + 5, 173, y + 11); //кончик
 	#endif
@@ -50,22 +50,22 @@ void show_statusbar(int y, int bgColor,int fgColor){
 			word battery_percentage = *((word*)(0x20000334));
 	#endif
 			#ifdef BATTERY_ICON
-			//Цвет индикатора батареи
+			//Цвет индикатора батареи Colors battery indicator
 			char r_count = battery_percentage / 33;
 			r_count = r_count > 2 ? 2 : r_count < 1 ? 0 : r_count; // if r_count > 2 = 2 elseif r_count < 1 = 0 else r_count
-				if (battery_percentage > 32) {
+				if (battery_percentage > 32) { //set color
 					set_bg_color(battery_percentage <= 65 ? COLOR_YELLOW : COLOR_GREEN);
 				}else if (battery_percentage <= 32) {
 					set_bg_color(COLOR_RED);
 				}
-
+				//draw cells
 				for (char i = 0; i <= r_count; i++)
 				{
 					draw_filled_rect_bg(155 + i * 5, y + 2, 159 + i * 5, y + 14);
 				}
 			#endif
 			#ifdef BATTERY_TEXT
-			//Проценты батареи
+			//Проценты батареи Battery in percents
 			char d1[4];     // переменная для перевода переменной для печати                               
 			_sprintf(d1, "%01d", battery_percentage); // конвертируем
 			set_bg_color(bgColor); // делаем фон
@@ -87,73 +87,70 @@ set_bg_color(bgColor); // делаем фон
 	text_out_center(clock_time, 88,y);		// печатаем результат(время)
 #endif
 #ifdef DATA
-	char data[12]; 			//	текст дата		день.месяц
+	char data[12]; 			//	текст дата	день.месяц     text date day.mounth
 	#ifndef TIME
 		struct datetime_ dt;
 		get_current_date_time(&dt);
 	#endif
 	_sprintf(data, "%02d.%02d", dt.day, dt.month);
-	text_out(data,4,y);		// печатаем день недели
+	text_out(data,4,y);		// печатаем print 
 #endif
 #ifdef BLUETOOTH
-	// БЛЮТУЗ
+	// БЛЮТУЗ 
 	char last_bt_con;
 	#ifdef BipEmulator
 		last_bt_con = 0;
 	#else
 		last_bt_con = check_app_state(0x200);
 	#endif
+		set_bg_color(COLOR_BLACK);
+		draw_filled_rect_bg(48, y-1, 62, y+17);
 	if (last_bt_con){
 		//paint bluetooth ON
-		set_bg_color(COLOR_BLACK);
-		draw_filled_rect_bg(48, y-1, 62, y+17);
 		set_bg_color(COLOR_BLUE);
-		draw_filled_rect_bg(49, y, 61, y+16);
 		set_fg_color(COLOR_WHITE);
-		text_out_center("ᛒ", 55, y);
 	}else{			
 		//paint bluetooth OFF
-		set_bg_color(COLOR_BLACK);
-		draw_filled_rect_bg(48, y-1, 62, y+17);
 		set_bg_color(COLOR_RED);
-		draw_filled_rect_bg(49, y, 61, y+16);
 		set_fg_color(COLOR_BLACK);
-		text_out_center("ᛒ", 55, y);
 	}
+		draw_filled_rect_bg(49, y, 61, y+16);
+		text_out_center("ᛒ", 55, y);
 #endif
 
-// Будильник
+// Будильник Ring
 #ifdef ALARM_CLOCK_RING
-	set_bg_color(COLOR_BLACK);
-	draw_filled_rect_bg(110, y-1, 117, y+16);
+	set_bg_color(COLOR_BLACK); //подложка
+	draw_filled_rect_bg(110, y, 121, y+16);
 	set_bg_color(COLOR_RED);
-	draw_filled_rect_bg(111, y+6, 116, y+15);
-	draw_filled_rect_bg(111, y, 116, y+3);
+	draw_filled_rect_bg(111, y+5, 120, y+15); //корпус
+	draw_filled_rect_bg(111, y+1, 120, y+4); //звонок
 	set_bg_color(COLOR_WHITE);
-	draw_filled_rect_bg(112, y+7, 115, y+14);
-	set_bg_color(COLOR_BLACK);
-	draw_filled_rect_bg(114, y+7, 114, y+10);
-	draw_filled_rect_bg(114, y+10, 115, y+10);
+	draw_filled_rect_bg(112, y+6, 119, y+14); //циферблат
+	set_bg_color(COLOR_BLACK); //стрелки
+	draw_filled_rect_bg(115, y+6, 116, y+10);
+	draw_filled_rect_bg(115, y+10, 119, y+11);
 #endif
 #ifdef ALARM_CLOCK
-	set_bg_color(COLOR_BLACK);
-	draw_filled_rect_bg(110, y-1, 117, y+16);
+	set_bg_color(COLOR_BLACK);//подложка
+	draw_filled_rect_bg(110, y-1, 121, y+16);
 	set_bg_color(COLOR_RED);
-	draw_filled_rect_bg(111, y, 116, y+15);
+	draw_filled_rect_bg(111, y, 120, y+15);//корпус
 	set_bg_color(COLOR_WHITE);
-	draw_filled_rect_bg(112, y+1, 115, y+14);
-	set_bg_color(COLOR_BLACK);
-	draw_filled_rect_bg(114, y+1, 114, y+8);
-	draw_filled_rect_bg(114, y+8, 115, y+8);
+	draw_filled_rect_bg(112, y+1, 119, y+14);//циферблат
+	set_bg_color(COLOR_BLACK);//стрелки
+	draw_filled_rect_bg(115, y+1, 116, y+8);
+	draw_filled_rect_bg(115, y+8, 119, y+9);
 #endif
 #ifdef ALARM_RING
-	set_bg_color(COLOR_BLACK);
-	draw_filled_rect_bg(112, y-2, 115, y);
-	draw_filled_rect_bg(111, y, 116, y+16);
-	draw_filled_rect_bg(110, y+11, 117, y+16);
-	set_bg_color(COLOR_YELLOW);
-	draw_filled_rect_bg(112, y+1, 115, y+15);
-	draw_filled_rect_bg(111, y+12, 116, y+15);
+	set_bg_color(COLOR_BLACK);//подложка
+	draw_filled_rect_bg(112, y-1, 119, y+1);  //вверх
+	draw_filled_rect_bg(111, y+1, 120, y+16); //основа
+	draw_filled_rect_bg(110, y+11, 121, y+16); //низ
+	set_bg_color(COLOR_YELLOW); //звонок
+	draw_filled_rect_bg(113, y, 118, y+1);  //вверх
+	draw_filled_rect_bg(112, y+2, 119, y+15);  //основа
+	draw_filled_rect_bg(111, y+12, 120, y+15); //низ
 #endif
 
 // РАЗДЕЛИТЕЛЬ
